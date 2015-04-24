c4186c81c419ef18db28e707c59f226b9d38c592
iScorpio <iura_scorpio@outlook.com>
2015-04-24 13:05:04 +0400

    Revert "Add x_auth_access_type for Twitter resource owner (require read/write permission despite of app-level settings)."
    This reverts commit f1cca622d82ef0ce6584293921882b854148856f.

--- OAuth/ResourceOwner/TwitterResourceOwner.php
+++ OAuth/ResourceOwner/TwitterResourceOwner.php
@@ -43,13 +43,5 @@ class TwitterResourceOwner extends GenericOAuth1ResourceOwner
             'access_token_url'  => 'https://api.twitter.com/oauth/access_token',
             'infos_url'         => 'https://api.twitter.com/1.1/account/verify_credentials.json',
         ));
-        
-        $resolver->setOptional(array(
-            'x_auth_access_type',
-        ));
-        $resolver->setAllowedValues(array(
-            // @link https://dev.twitter.com/oauth/reference/post/oauth/request_token
-            'x_auth_access_type' => array('read', 'write'),
-        ));
     }
 }
